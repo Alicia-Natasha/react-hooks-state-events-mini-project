aa75d290483f35f3a866be41bc22e4e983ec7c83
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-13 02:04:34 +0300

    react-hooks-state-events-mini-project

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text, category });
+    setText("");
+    setCategory("Produce");
+  }
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
+    <form className="new-task-form" onSubmit={handleSubmit}>
+    <label htmlFor="task-details">Details</label>
+    <input
+      id="task-details"
+      type="text"
+      name="text"
+      placeholder="New task details"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+    <label htmlFor="category-select">Category</label>
+    <select
+      id="category-select"
+      name="category"
+      value={category}
+      onChange={(e) => setCategory(e.target.value)}
+    >
+      {categories
+        .filter((cat) => cat !== "All")
+        .map((cat) => (
+          <option key={cat} value={cat}>
+            {cat}
+          </option>
+        ))}
+    </select>
+    <input type="submit" value="Add task" />
+  </form>
+  
   );
 }
 
